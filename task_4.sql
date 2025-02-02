6757824a192fa0643d1bd5945985d4f3ad1009f8
Ghostlyjeff <odifirijide1234@gmail.com>
2025-02-02 23:31:50 +0100

    new changes

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,8 @@
 -- Use the database
 USE alx_book_store;
 
--- Get full description of the books table
-SHOW CREATE TABLE Books;
+-- Get full description of the books table using INFORMATION_SCHEMA
+SELECT COLUMN_NAME, COLUMN_TYPE 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+AND TABLE_NAME = 'Books';
